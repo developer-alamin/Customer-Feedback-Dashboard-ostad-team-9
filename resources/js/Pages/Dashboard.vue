b5c1131605d35c7e9bab9fcd79a401fd4c176d81
shifkatahamed522 <shifkatahamed522@gmail.com>
2025-07-01 10:43:45 +0600

    4 card on top

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -26,5 +26,67 @@ import { Head } from '@inertiajs/vue3';
                 </div>
             </div>
         </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3" >
+                    <div class="card card-plain h-100 bg-white p-3 text-center">
+                        <p class="mb-2 text-sm text-gray-500">Survey Send</p>
+                        <h2 class="mb-0 text-capitalize font-weight-bold">
+                             <span id="product">60</span>
+                         </h2>
+                                    
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-plain h-100 bg-white p-3 text-center">
+                        <p class="mb-2 text-sm text-gray-500">Response Received</p>
+                        <h2 class=" mb-0 text-capitalize font-weight-bold">
+                             <span id="product">42</span>
+                         </h2>
+                                    
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-plain h-100 bg-white p-3 text-center">
+                        <p class="mb-2 text-sm text-gray-500">Response Pending</p>
+                        <h2 class="mb-0 text-capitalize font-weight-bold">
+                             <span id="product">12</span>
+                         </h2>
+                                    
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card card-plain h-100 bg-white p-3 text-center">
+                        <p class="mb-2 text-sm text-gray-500">Customer Excluded</p>
+                        <h2 class="mb-0 text-capitalize font-weight-bold">
+                             <span id="product">06</span>
+                         </h2>
+                                    
+                    </div>
+                </div>
+            </div>
+        </div>
     </AuthenticatedLayout>
 </template>
+
+<style scoped>
+.text-green{
+    color:#41b883 !important;
+}
+
+.card{
+    border: none !important;
+}
+
+.btn-success{
+    background: #41b883 !important;
+    border-color:  #41b883 !important;
+}
+.center-screen{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 85vh;
+}
+</style>
