bbfd2861ebcf6b9b86c2f57e460705ce665a5015
shifkatahamed522 <shifkatahamed522@gmail.com>
2025-07-02 15:49:27 +0600

    4 card on top

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -18,9 +18,54 @@ const props = defineProps({
             <div class="flex justify-between gap-8">
                 
                 <!-- Left Side : Main Content Area -->
-                <div class="w-[73%] overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="w-[73%] overflow-hidden shadow-sm sm:rounded-lg">
+
                     <div class="p-6 text-gray-900">You're logged in!</div>
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-3" >
+                                <div class="card card-plain h-100 bg-white p-3 text-center">
+                                    <p class="mb-2 text-sm text-gray-500">Survey Send</p>
+                                    <h2 class="mb-0 text-capitalize font-weight-bold">
+                                        <span id="product">60</span>
+                                    </h2>
+                                                
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="card card-plain h-100 bg-white p-3 text-center">
+                                    <p class="mb-2 text-sm text-gray-500">Response Received</p>
+                                    <h2 class=" mb-0 text-capitalize font-weight-bold">
+                                        <span id="product">42</span>
+                                    </h2>
+                                                
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="card card-plain h-100 bg-white p-3 text-center">
+                                    <p class="mb-2 text-sm text-gray-500">Response Pending</p>
+                                    <h2 class="mb-0 text-capitalize font-weight-bold">
+                                        <span id="product">12</span>
+                                    </h2>
+                                                
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="card card-plain h-100 bg-white p-3 text-center">
+                                    <p class="mb-2 text-sm text-gray-500">Customer Excluded</p>
+                                    <h2 class="mb-0 text-capitalize font-weight-bold">
+                                        <span id="product">06</span>
+                                    </h2>
+                                                
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
+
+                
 
                 <!-- Right Side : Questions Answer Area -->
                 <div class="w-[27%]">
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import '../css/app.css';
 import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
